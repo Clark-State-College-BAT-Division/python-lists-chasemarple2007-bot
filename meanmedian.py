bb4c6dd37589ea0b5d8823d9ec872043aa538fc3
Chase Marple <chasemarple2007@gmail.com>
2025-10-01 13:30:27 -0400

    Update meanmedian.py

--- meanmedian.py
+++ meanmedian.py
@@ -3,3 +3,23 @@
 #Calculate and display the mean and median
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
+
+
+myNumbers = [0,0,0,0,0]
+
+for i in range(5):
+  myNumbers[i] = int(input("Enter a number: "))
+
+print(myNumbers)
+myNumbers.sort()
+print(myNumbers)
+myNumbers.sort(reverse=True)
+print(myNumbers)
+
+sum = 0
+for i in range(5):
+  sum += myNumbers[i]
+
+average = sum / 5
+
+print(f"The mean is {average} and the median is {myNumbers[2]}")
